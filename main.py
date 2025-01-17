01029d9618396b85657395c22698f29e7f3ab8f2
Norbert <54719382+NorbertCode@users.noreply.github.com>
2025-01-17 17:08:50 +0100

    dynamic page checking

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import requests
+import math
 
 
-competitions = []
-for page in range(1, 18):
-    comp_data = requests.get(f"https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/competitions-page-{page}.json").json()
-    comps_names = [comp["id"] for comp in comp_data["items"]]
-    competitions.extend(comps_names)
-pass
+def get_total_pages(url) -> int:
+    comp_data = requests.get(url).json()
+    return math.ceil(comp_data["total"] / comp_data["pagination"]["size"])
+
+
+def get_competition_names() -> list[str]:
+    competitions = []
+    total_pages = get_total_pages("https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/competitions.json")
+    for page in range(1, total_pages + 1):
+        comp_data = requests.get(f"https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/competitions-page-{page}.json").json()
+        comps_names = [comp["id"] for comp in comp_data["items"]]
+        competitions.extend(comps_names)
+    return competitions
+
+
+get_competition_names()
